cbeaccd0bbdcf0633ac20947fb51f1c92e78e56d
Tafon <tafonsoftwarespecialist@gmail.com>
2023-11-14 23:27:06 +0100

    starting the react app

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,15 +1,14 @@
-import Navbar from "./components/navbar";
 
+import './App.css';
 
 function App() {
-  return(
-
-    <>
-      <Navbar />
-    </>
-    
-    
-  )
-};
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+      </h1>
+    </div>
+  );
+}
 
 export default App;
