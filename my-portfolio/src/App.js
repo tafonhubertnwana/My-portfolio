eb7d7c0054c1361499e6594a898317c119626deb
Tafon <tafonsoftwarespecialist@gmail.com>
2023-11-15 17:13:53 +0100

    completing the navbar

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Navbar from "./components/Navbar";
+import Home from "./components/Home"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       
 
       <Navbar />
+      <Home />
     </div>
   );
 }
