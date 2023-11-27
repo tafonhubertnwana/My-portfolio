110d899c53a36c950b8fc1e7795e317b93a599e8
Tafon <tafonsoftwarespecialist@gmail.com>
2023-11-28 00:26:33 +0100

    structuring the experience section

--- my-portfolio/src/components/Experience.jsx
+++ my-portfolio/src/components/Experience.jsx
@@ -6,11 +6,28 @@ import javascript from "../image/javascriptLogo.png"
 import node from "../image/nodejs.png"
 import tailwind from "../image/tailwind.png"
 import react from "../image/reactjsLogo.jpg"
+import github from "../image/github.jpg"
 
 const Experience= () => {
   return (
-    <div>
-      
+    <div name="experience">
+      <div>
+        <div>
+          <p>
+            Experience
+          </p>
+          <p>
+            These are the technology i have worked on
+          </p>
+        </div>
+
+        <div>
+          <div>
+            <img src={html} alt="" />
+            <p>HTML</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
