0b491c95c303c9495acf845c1026a6173c06fae8
Tafon <tafonsoftwarespecialist@gmail.com>
2023-11-27 22:37:46 +0100

    modifying the porfolio section

--- my-portfolio/src/components/Portfolio.jsx
+++ my-portfolio/src/components/Portfolio.jsx
@@ -9,17 +9,17 @@ const Portfolio = () => {
       src: image
     },
     {
-      id: 1,
+      id: 2,
       src: image
     },
     {
-      id: 1,
+      id: 3,
       src: image
     },
     {
-      id: 1,
+      id: 4,
       src: image
-    },
+    }
 
   ]
 
@@ -35,15 +35,22 @@ const Portfolio = () => {
           <p className='py-6'>Check our some of my work right here</p>
         </div>
 
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12'>
-          <div>
-            <img src={image} alt="" className='rounded-md duration-200 hover:scale-105'/>
-            <div className='flex items-center justify-center'>
-              <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
-              <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
+
+          {portfolios.map(({id, src}) => (
+            
+            <div key= {id} className='shadow-md shadow-gray-600 rounded-lg'>
+              <img src={src} alt="" className='rounded-md duration-200 hover:scale-105'/>
+              <div className='flex items-center justify-center'>
+                <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
+                <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
+        
+
+        
       </div>
     </div>
   )
