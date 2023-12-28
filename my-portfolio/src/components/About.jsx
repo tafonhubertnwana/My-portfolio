4409178d7677b98bbf1b9d0441d34a4a859347ae
Tafon <tafonsoftwarespecialist@gmail.com>
2023-12-28 18:26:10 +0100

    changing link

--- my-portfolio/src/components/About.jsx
+++ my-portfolio/src/components/About.jsx
@@ -12,13 +12,13 @@ const About = () => {
           </div>
 
           <p className="text-xl mt-10">
-            I am Full Stack Developer with 6 years of experience on both the frontend and the backend. Over the years I have worked with clients to develop, manage, and maintain full stack applications leveraging cloud native technologies. I have extensive experience with a wide range of programming languages and development frameworks, including JavaScript, HTML, CSS, Python, and java. I have also worked with popular web development frameworks such as Angular.js, React.js, and Vue.js. I am also proficient in working with database such as MySQL, Postgres, and MongoDB.
+            I am Full Stack Developer with experience on both the frontend and the backend. Over the years I have worked with clients to develop, manage, and maintain full stack applications leveraging cloud native technologies. I have extensive experience with a wide range of programming languages and development frameworks, including JavaScript, HTML, CSS, Python, and java. I have also worked with popular web development frameworks such as Angular.js, React.js, and Vue.js. I am also proficient in working with database such as MySQL, Postgres, and MongoDB.
           </p>
 
           <br />
 
           <p className="text-xl">
-            In my current position as  a Full Stack Developer, I develop web applications from ground up and maintain existing applications stack to ensure they meet user requirements. I also collaborate with other teams to ensure the project is delivered on time. I also have experience with version control systems such as Git, which I use to track changes in code. I am comfortable working in both agile and DevOps methods, and I have a strong understanding of software engineering principle.
+            I develop web applications from ground up and maintain existing applications stack to ensure they meet user requirements. I also collaborate with other teams to ensure the project is delivered on time. I also have experience with version control systems such as Git, which I use to track changes in code. I am comfortable working in both agile and DevOps methods, and I have a strong understanding of software engineering principle.
             I am a creative problem solver and I enjoy working with complex challenges. My ability to learn quickly and adapt to new technologies makes me a valuable asset. I also have excellent communication and organizational skills, which help me to collaborate effectively with other team members.
           </p>
         </div>
